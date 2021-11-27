79769b6d3d98f8a45ad31626a52deeae5eef821b
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2021-11-28 02:12:57 +0900

    create layout component

---
+++ src/components/ui-elements/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <nav className="flex bg-blue-900 flex-between items-center text-white">
+        <div className="flex-grow font-bold pl-5">
+          <Link href="/">
+            <a>ASK</a>
+          </Link>
+        </div>
+        <div className="block p-4 flex-none hover:bg-blue-800">
+          <Link href="/#">
+            <a>All Posts</a>
+          </Link>
+        </div>
+        <div className="block p-4  flex-none hover:bg-blue-800">
+          <Link href="/#">
+            <a>+</a>
+          </Link>
+        </div>
+      </nav>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
