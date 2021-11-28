49a7d4c07f021d66a693236d4df932d581e30621
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2021-11-28 22:16:47 +0900

    fix css for layout

--- src/components/ui-elements/Layout.tsx
+++ src/components/ui-elements/Layout.tsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
-      <nav className="flex bg-blue-500 flex-between items-center text-white">
+      <nav className="flex bg-blue-500 justify-end items-center text-white">
         <div className="flex-grow font-bold pl-5">
           <Link href="/">
             <a>ASK</a>
           </Link>
         </div>
-        <div className="block p-4 flex-none hover:bg-blue-700">
+        <div className="block p-4 hover:bg-blue-700">
           <Link href="/#">
             <a>All Posts</a>
           </Link>
         </div>
-        <div className="block p-4  flex-none hover:bg-blue-700">
+        <div className="block p-4 hover:bg-blue-700">
           <Link href="/#">
             <a>+</a>
           </Link>
