be12eb6586effa35b5e9c4111172dcb9dd68e700
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2021-12-06 02:21:58 +0900

    create index for layout and fix import statement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import Layout from '../components/ui-elements/Layout';
+import { Layout } from '../components/layout';
 import { Posts } from '../components/posts';
 
 const Page: NextPage = () => {
