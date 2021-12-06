3fc5a8ebb1c8e63c55488ccfd5d80eda7219c286
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2021-12-06 20:55:00 +0900

    implement func to create a new post

--- src/components/newPost/newPost.container.tsx
+++ src/components/newPost/newPost.container.tsx
@@ -1,10 +1,14 @@
 import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import { Layout } from '../layout';
 import { NewPostComponent } from './newPost';
+import { Web3Container } from 'src/container/web3Container';
+import { createQuestion } from '../../utils/contractHelper';
 
 export type FormData = {
   title: string;
   description: string;
-  reward: number;
+  reward: string;
   restrict: boolean;
 };
 
@@ -15,11 +19,41 @@ export const NewPost: React.FC = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = handleSubmit((data: FormData) => {
-    console.log(data);
+  const router = useRouter();
+  const { web3 } = Web3Container.useContainer();
+
+  const onSubmit = handleSubmit(async (data: FormData) => {
+    try {
+      if (web3) {
+        const contents = {
+          title: data.title,
+          description: data.description,
+          restrict: data.restrict,
+        };
+        const hex = web3.utils.stringToHex(JSON.stringify(contents));
+
+        await createQuestion(
+          web3,
+          parseInt(web3.utils.toWei(data.reward, 'ether')),
+          hex
+        );
+
+        router.push('/');
+      } else {
+        // TODO handle error
+      }
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   return (
-    <NewPostComponent onSubmit={onSubmit} register={register} errors={errors} />
+    <Layout>
+      <NewPostComponent
+        onSubmit={onSubmit}
+        register={register}
+        errors={errors}
+      />
+    </Layout>
   );
 };
