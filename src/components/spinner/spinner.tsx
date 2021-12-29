c0fcca82b81e7dc574c445316dd2e618f530c245
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2021-12-29 21:09:27 +0900

    create loading spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,7 @@
+export const SpinnerComponent = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="animate-spin h-8 w-8 bg-blue-500 rounded-xl"></div>
+    </div>
+  );
+};
